2994e2e3bf0bd86c0682d4d25a3aa4e9d92516b1
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-11 20:09:40 +0000

    Removes log of DB_URL

--- src/database/buildsdb.js
+++ src/database/buildsdb.js
@@ -1,8 +1,5 @@
 const fs = require('fs');
-
 const dbConn = require('./db_connection');
-console.log(process.env.DB_URL);
-
 const sql = fs.readFileSync(`${__dirname}/builds.sql`).toString();
 
 dbConn.query(sql, (error, result) => {

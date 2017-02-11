f04592ffeeaa8b60cfa98ce58a2f31afcaacc9b0
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-11 13:02:47 +0000

    Adds curly braces where missing

--- src/database/queries/get_accounts.js
+++ src/database/queries/get_accounts.js
@@ -1,7 +1,10 @@
 const dbConn = require('../db_connection.js');
 
-module.exports = cb =>
-console.log("hi");
-  dbConn.query('SELECT * FROM accounts;', (error, data) =>
-  console.log("hi again")
-    (error ? cb(error) : cb(null, data.rows)));
+module.exports = cb => {
+  console.log("hi");
+  dbConn.query('SELECT * FROM accounts;', (error, data) => {
+      console.log("hi again");
+      error ? cb(error) : cb(null, data.rows);
+    }
+  );
+}

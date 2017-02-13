ff3621b49e843e367917089b510638c44d5dfb04
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-13 16:29:23 +0000

    Creates new post route for new payment

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const getAccounts = require('./database/queries/get_accounts.js');
 const getTransactions = require('./database/queries/get_transactions.js');
+const makeTransfer = require('./database/queries/make_transfer.js');
 
 const general = {
   method: 'GET',
@@ -27,9 +28,25 @@ const payments = {
   method: 'GET',
   path: '/payments',
   handler:(req, reply) => {
-    reply.view('payments');
+    reply.view('payments', {transaction_id: req.query.transaction_id});
   }
-}
+};
+
+const newPayment = {
+  method: 'POST',
+  path: '/new-payment',
+  config: {
+    handler: (req, reply) => {
+      console.log(req.payload);
+      makeTransfer(req.payload, error => {
+        console.log("req.payload:"+req.payload);
+        if (error) console.log('Error:', error);
+        console.log("back in router");
+        reply.redirect('/');
+      });
+    }
+  }
+};
 
 const accounts = {
   method: 'GET',
@@ -47,5 +64,6 @@ module.exports = [
   general,
   home,
   payments,
+  newPayment,
   accounts
 ];

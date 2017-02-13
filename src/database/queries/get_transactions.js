706487cc8716a6a5b559e0856ed7288906a7b0cf
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-13 13:09:46 +0000

    Reverses order of transaction history so most recent displays first

--- src/database/queries/get_transactions.js
+++ src/database/queries/get_transactions.js
@@ -2,10 +2,11 @@ const dbConn = require('../db_connection.js');
 
 module.exports = (cb) => {
   console.log('Running get_transactions.js');
-  dbConn.query('SELECT * \
-                FROM transactions\
-                WHERE acc_id_from = 1 OR\
-                acc_id_to = 1;',
+  dbConn.query(`SELECT *
+                FROM transactions
+                WHERE acc_id_from = 1 OR
+                acc_id_to = 1
+                ORDER BY tran_date DESC;`,
                 (error, result) => {
       console.log('Selecting all from transactions');
       error ? cb(error) : cb(null, result.rows);

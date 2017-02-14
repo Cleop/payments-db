1cbe8161dbd3e16016eedc3156f8e9baf8fb9bf9
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-14 17:20:25 +0000

    Removes unneeded logs

--- src/database/queries/get_accounts.js
+++ src/database/queries/get_accounts.js
@@ -1,7 +1,6 @@
 const dbConn = require('../db_connection.js');
 
 module.exports = cb => {
-  console.log('Running get_accounts.js');
   dbConn.query(`SELECT t.acc_id_to, a.acc_name, t.balance
       FROM (
           SELECT acc_id_to, SUM(transactions.amount) as balance
@@ -10,7 +9,6 @@ module.exports = cb => {
         ) t
       JOIN accounts a ON t.acc_id_to = a.acc_id`,
       (error, data) => {
-      console.log('Selecting all from accounts');
       error ? cb(error) : cb(null, data.rows);
     }
   );
--- src/database/queries/get_transactions.js
+++ src/database/queries/get_transactions.js
@@ -1,7 +1,6 @@
 const dbConn = require('../db_connection.js');
 
 module.exports = (acc_id_to, cb) => {
-  console.log('Running get_transactions.js');
   dbConn.query(`SELECT t.acc_id_from, a.acc_name, t.amount,
                 CAST(t.tran_date AS CHAR(10)) short_date
                 FROM transactions t
@@ -10,7 +9,6 @@ module.exports = (acc_id_to, cb) => {
                 ORDER BY tran_date DESC;`,
                 [acc_id_to],
                 (error, result) => {
-      console.log('Selecting all from transactions');
       error ? cb(error) : cb(null, result.rows);
     });
 };

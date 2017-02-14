53313f2417800e8f56523725bd8b8ca7589393a9
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-14 09:19:19 +0000

    Adds amount columns to generate balance

--- src/database/queries/get_accounts.js
+++ src/database/queries/get_accounts.js
@@ -2,7 +2,14 @@ const dbConn = require('../db_connection.js');
 
 module.exports = cb => {
   console.log('Running get_accounts.js');
-  dbConn.query('SELECT * FROM accounts;', (error, data) => {
+  dbConn.query(`SELECT t.acc_id_to, a.acc_name, t.balance
+      FROM (
+          SELECT acc_id_to, SUM(transactions.amount) as balance
+          FROM transactions
+          GROUP BY transactions.acc_id_to
+        ) t
+      JOIN accounts a ON t.acc_id_to = a.acc_id`,
+      (error, data) => {
       console.log('Selecting all from accounts');
       error ? cb(error) : cb(null, data.rows);
     }

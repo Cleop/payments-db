1afc706bf2ce5c53c1b301215abc2764c4d1a989
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-14 17:17:27 +0000

    Amends log to make it clearer

--- src/database/buildsdb.js
+++ src/database/buildsdb.js
@@ -3,5 +3,5 @@ const dbConn = require('./db_connection');
 const sql = fs.readFileSync(`${__dirname}/builds.sql`).toString();
 
 dbConn.query(sql, (error, result) => {
-  (error ? console.log('Error receiving builds.sql', error) : console.log('Result', result));
+  (error ? console.log('Error receiving builds.sql', error) : console.log('Result:', result));
 });

d9bb0319efb0fac583382a371d752181e55412a3
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-14 15:54:58 +0000

    Converts date to shortened date without time

--- src/database/queries/get_transactions.js
+++ src/database/queries/get_transactions.js
@@ -2,8 +2,10 @@ const dbConn = require('../db_connection.js');
 
 module.exports = (acc_id_to, cb) => {
   console.log('Running get_transactions.js');
-  dbConn.query(`SELECT *
-                FROM transactions
+  dbConn.query(`SELECT t.acc_id_from, a.acc_name, t.amount,
+                CAST(t.tran_date AS CHAR(10)) short_date
+                FROM transactions t
+                JOIN accounts a ON t.acc_id_from = a.acc_id
                 WHERE acc_id_to = $1
                 ORDER BY tran_date DESC;`,
                 [acc_id_to],
@@ -12,3 +14,5 @@ module.exports = (acc_id_to, cb) => {
       error ? cb(error) : cb(null, result.rows);
     });
 };
+
+// SELECT CAST(tran_date AS DATE) FROM transactions

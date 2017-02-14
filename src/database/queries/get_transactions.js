186e8bd70da3c1c6b66abf7ea8ecd80947e9424f
Cleop <cleo.pearson@hotmail.co.uk>
2017-02-14 09:20:09 +0000

    Adds variable for account number so query can be used for numerous accounts

--- src/database/queries/get_transactions.js
+++ src/database/queries/get_transactions.js
@@ -1,12 +1,12 @@
 const dbConn = require('../db_connection.js');
 
-module.exports = (cb) => {
+module.exports = (acc_id_to, cb) => {
   console.log('Running get_transactions.js');
   dbConn.query(`SELECT *
                 FROM transactions
-                WHERE acc_id_from = 1 OR
-                acc_id_to = 1
+                WHERE acc_id_to = $1
                 ORDER BY tran_date DESC;`,
+                [acc_id_to],
                 (error, result) => {
       console.log('Selecting all from transactions');
       error ? cb(error) : cb(null, result.rows);
